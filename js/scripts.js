4dc1a12a68acae7c53db9750a15c70c519c829cd
Allie Bryan <allythecat79@gmail.com>
2019-09-02 20:37:29 -0700

    added liters to gallons on scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var convert = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter the amount of liters:"));
+var number2 = parseFloat(3.785);
+
+alert(convert(number1, number2));
