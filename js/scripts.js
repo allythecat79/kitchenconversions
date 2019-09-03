b59d2daef83dc13f7453136eadbed80d0cbdd846
Allie Bryan <allythecat79@gmail.com>
2019-09-02 20:52:33 -0700

    add cups to liters

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
-var convert = function(number1, number2) {
+var litersToGallons = function(number1, number2) {
   return number1 * number2;
 };
 
 var number1 = parseInt(prompt("Enter the amount of liters:"));
 var number2 = parseFloat(3.785);
 
-alert(convert(number1, number2));
+alert(litersToGallons(number1, number2));
+
+var cupsToMl = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter the amount of cups:"));
+var number2 = parseInt(250);
+
+alert(cupsToMl(number1, number2));
