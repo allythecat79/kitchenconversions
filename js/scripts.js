7304bd4a894a67da99d8e9b7f18da37eea363a1c
Allie Bryan <allythecat79@gmail.com>
2019-09-04 08:57:00 -0700

    added ounces to cups

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,12 @@ var number1 = parseInt(prompt("Enter the amount of cups:"));
 var number2 = parseInt(250);
 
 alert(cupsToMl(number1, number2));
+
+var ouncesToCups = function(number1, number2)  {
+  return number1 / number2;
+}
+
+var number1 = parseInt(prompt("Enter the amount of ounces:"));
+var number2 = parseInt(8);
+
+alert(ouncesToCups(number1,number2));
